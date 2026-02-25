78be1389a513c94061da50a6010e748496d690cd
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-25 10:31:33 +0100

    Favoritos?

--- OpenAni_DB/Users/serializers/favorito_serializer.py
+++ OpenAni_DB/Users/serializers/favorito_serializer.py
@@ -4,14 +4,14 @@ from Users.models import FavoritoModel
 
 
 class FavoritoSerializer(serializers.ModelSerializer):
-    nombre_usuario = serializers.CharField(required=True)
-    nombre_torrent = serializers.CharField(required=True)
-    enlace = serializers.CharField(required=True)
-    tamano = serializers.CharField(required=True)
-    fecha = serializers.CharField(required=True)
+    nombre_usuario = serializers.CharField(required=False)
+    title = serializers.CharField(required=True)
+    magnet = serializers.CharField(required=True)
+    size = serializers.CharField(required=True)
+    time = serializers.CharField(required=True)
     seeders = serializers.CharField(required=True)
     leechers = serializers.CharField(required=True)
 
     class Meta:
         model = FavoritoModel
-        fields = ("nombre_usuario", "nombre_torrent", "enlace", "tamano", "fecha", "seeders", "leechers")
+        fields = ("nombre_usuario", "title", "magnet", "size", "time", "seeders", "leechers")
--- OpenAni_DB/Users/views/favorito_view.py
+++ OpenAni_DB/Users/views/favorito_view.py
@@ -1,3 +1,4 @@
+from django.db.migrations import serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,7 +15,7 @@ class FavoritoView(APIView):
         if nombre_user_query:
             favoritos = FavoritoModel.objects.filter(nombre_usuario__icontains=nombre_user_query)
             serializer = FavoritoSerializer(favoritos, many=True)
-            return Response({"favoritos": serializer.data}, status=status.HTTP_200_OK)
+            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
         return Response(serializer.data)
 
     def post(self, request):
@@ -26,3 +27,15 @@ class FavoritoView(APIView):
         else:
             print(serializer.errors)
             return Response({"Errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        favoritos = FavoritoModel.objects.all()
+        nombre_user_query = request.query_params.get('nombre_usuario', None)
+        magnet_query = request.query_params.get('magnet', None)
+        if nombre_user_query and magnet_query:
+            torrentspornombre = favoritos.filter(nombre_usuario__icontains=nombre_user_query)
+            torrentspormagnet = torrentspornombre.filter(magnet=magnet_query)
+            if torrentspormagnet:
+                torrentspormagnet.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"Errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
